d5778dfcd7a63ac2e06143ae2c46247d9312fc86
IPKIN9 <irwandirini@gmail.com>
2022-05-30 14:15:40 +0800

    Create general controller from Auth, CMS, and WEB

---
+++ app/Http/Controllers/CMS/MasterController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\CMS;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class MasterController extends Controller
+{
+    public function index()
+    {
+    }
+
+    public function getAll()
+    {
+    }
+
+    public function getById()
+    {
+    }
+
+    public function updateById($id)
+    {
+    }
+
+    public function deleteById($id)
+    {
+    }
+}
