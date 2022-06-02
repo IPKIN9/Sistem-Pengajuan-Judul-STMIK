2c6988814dc6878fc2139db943a96a3df6d140c8
IPKIN9 <irwandirini@gmail.com>
2022-06-02 19:02:47 +0800

    Add: Create data interface function

--- app/Interfaces/MahasiswaRepositoryInterface.php
+++ app/Interfaces/MahasiswaRepositoryInterface.php
@@ -5,8 +5,8 @@ namespace App\Interfaces;
 interface MahasiswaRepositoryInterface
 {
     public function getAllMahasiswa();
-    public function getMahasiswaById($mahasiswa_id);
-    public function createMahasiswa(array $mahasiswa_details);
-    public function updateMahasiswaById($mahasiswa_id, array $new_details);
-    public function deleteMahasiswa($mahasiswa_id);
+    public function getMahasiswaById($mahasiswaId);
+    public function createMahasiswa(array $mahasiswaDetails);
+    public function updateMahasiswaById($mahasiswa_id, array $newDetails);
+    public function deleteMahasiswa($mahasiswaId);
 }
