069f2d9f372b70e6e73f393efc9e679de1a1e60e
IPKIN9 <irwandirini@gmail.com>
2022-06-04 18:47:44 +0800

    Get dosen by id repository function

--- app/Repositories/DosenRepository.php
+++ app/Repositories/DosenRepository.php
@@ -19,6 +19,38 @@ class DosenRepository implements DosenRepositoryInterface
 
     public function getDosenById($dosenId)
     {
+        try {
+            $dbResult = DosenModel::whereId($dosenId)->first();
+            if ($dbResult) {
+                $dosen = array(
+                    'data' => $dbResult,
+                    'message' => "Success",
+                    'code' => 200
+                );
+            } else {
+                $dosen = array(
+                    'data' => null,
+                    'response' => array(
+                        'icon' => 'warning',
+                        'title' => 'Not Found',
+                        'message' => 'Data tidak tersedia',
+                    ),
+                    'code' => 404
+                );
+            }
+        } catch (\Throwable $th) {
+            $dosen = array(
+                'data' => null,
+                'response' => array(
+                    'icon' => 'error',
+                    'title' => 'Gagal',
+                    'message' => $th->getMessage(),
+                ),
+                'code' => 500
+            );
+        }
+
+        return $dosen;
     }
 
     public function updateDosenById($dosenId, array $newDetails)
