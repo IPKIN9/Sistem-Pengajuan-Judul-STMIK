caa384989ec07dbf2d3b01458209dc295af60767
husnulmaniah <husnulmaniah86@gmail.com>
2022-06-04 15:16:01 +0800

    update belum fix

--- resources/views/CMS/mahasiswa.blade.php
+++ resources/views/CMS/mahasiswa.blade.php
@@ -60,10 +60,11 @@
                                         <td>{{ $d->angkatan }}</td>
                                         <td>{{ $d->kelas }}</td>
                                         <td>
-                                            <a href="" type="button"
-                                                class="btn rounded-pill btn-outline-primary">Edit</a>
-                                            <a href="" type="button" id="deleteId"
-                                                class="btn rounded-pill btn-outline-danger">Delete</a>
+                                            <button type="button" id="editId" data-id="{{$d->id}}"
+                                                data-bs-toggle="modal" data-bs-target="#modalUpdate"
+                                                class="btn rounded-pill btn-outline-primary">Edit</button>
+                                            <button type="button" id="deleteId" data-id="{{$d->id}}"
+                                                class="btn rounded-pill btn-outline-danger">Delete</button>
                                         </td>
                                     </tr>
                                     @endforeach
@@ -175,54 +176,181 @@
 @section('js')
 <script>
     $(document).ready(function() {
-            $.ajaxSetup({
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+
+        $('#table').DataTable();
+
+        $('#saveId').on('click', function() {
+            let url = "http://127.0.0.1:8000/api/mahasiswa/";
+            let data = $('#formSimpan').serialize();
+            $.ajax({
+                url: url,
+                method: "POST",
+                data: data,
+                success: function(result) {
+                    Swal.fire({
+                        title: result.response.title,
+                        text: result.response.message,
+                        icon: result.response.icon,
+                        cancelButtonColor: '#d33',
+                        confirmButtonText: 'Oke'
+                    }).then((result) => {
+                        location.reload();
+                    });
+                },
+                error: function(result) {
+                    let data = result.responseJSON
+                    let errorRes = data.errors
+                    Swal.fire({
+                        icon: data.response.icon,
+                        title: data.response.title,
+                        text: data.response.message,
+                    });
+                    if (errorRes.length >= 1) {
+                        $('.miniAlert').html('');
+                        $('#alertNama').html(errorRes.data.nama);
+                        $('#alertNim').html(errorRes.data.nim);
+                        $('#alertJurusan').html(errorRes.data.jurusan);
+                        $('#alertJk').html(errorRes.data.jk);
+                        $('#alertHp').html(errorRes.data.hp);
+                        $('#alertAlamat').html(errorRes.data.alamat);
+                        $('#alertAngkatan').html(errorRes.data.angkatan);
+                        $('#alertKelas').html(errorRes.data.kelas);
+                    }
                 }
             });
+        });
 
-            $('#table').DataTable();
+        $(document).on('click', '#editId', function(){
+            let dataId = $(this).data('id');
+            let url = {!! json_encode(url('/api/mahasiswa')) !!} + "/" + dataId;
+            $.ajax({
+                url: url,
+                type: 'GET',
+                success: function(result){ 
+                    console.log(result);
+                    let data = result.data;
+                    $('#modalUpdate').show('modal');
+                    $('.modal-title').html('Perubahan Data');
+                    $('#formUpdate').html('');
+                    $('#formUpdate').append(`
+                    <div class="row mb-3">
+                        <label class="col-sm-2 col-form-label" for="basic-default-name">Nama</label>
+                        <div class="col-sm-10">
+                            <input type="hidden" id="mahasiswaId" value="`+data.id+`">
+                            <input type="text" class="form-control" name="nama" value="`+data.nama+`"
+                                placeholder="Input Nama Lengkap">
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label class="col-sm-2 col-form-label" for="basic-default-name">NIM</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" name="nim" value="`+data.nim+`"
+                                placeholder="Input NIM">
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label class="col-sm-2 col-form-label"
+                            for="basic-default-message">Jurusan</label>
+                        <div class="col-sm-10">
+                            <select name="jurusan" id="jurusanUpdate" class="form-control">
+                                <option selected disabled>Pilih Jurusan</option>
+                                <option value="Teknik Informatika">Teknik Informatika</option>
+                                <option value="Sistem Informasi">Sistem Informasi</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label class="col-sm-2 col-form-label"
+                            for="basic-default-name">Handphone</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" name="hp" value="`+data.hp+`"
+                                placeholder="Input No Handphone">
+                        </div>
+                    </div>
 
-            $('#saveId').on('click', function() {
-                let url = "http://127.0.0.1:8000/api/mahasiswa/";
-                let data = $('#formSimpan').serialize();
-                $.ajax({
-                    url: url,
-                    method: "POST",
-                    data: data,
-                    success: function(result) {
-                        Swal.fire({
-                            title: result.response.title,
-                            text: result.response.message,
-                            icon: result.response.icon,
-                            cancelButtonColor: '#d33',
-                            confirmButtonText: 'Oke'
-                        }).then((result) => {
-                            location.reload();
-                        });
-                    },
-                    error: function(result) {
-                        let data = result.responseJSON
-                        let errorRes = data.errors
-                        Swal.fire({
-                            icon: data.response.icon,
-                            title: data.response.title,
-                            text: data.response.message,
-                        });
-                        if (errorRes.length >= 1) {
-                            $('.miniAlert').html('');
-                            $('#alertNama').html(errorRes.data.nama);
-                            $('#alertNim').html(errorRes.data.nim);
-                            $('#alertJurusan').html(errorRes.data.jurusan);
-                            $('#alertJk').html(errorRes.data.jk);
-                            $('#alertHp').html(errorRes.data.hp);
-                            $('#alertAlamat').html(errorRes.data.alamat);
-                            $('#alertAngkatan').html(errorRes.data.angkatan);
-                            $('#alertKelas').html(errorRes.data.kelas);
-                        }
-                    }
-                });
+                    <div class="row mb-3">
+                        <label class="col-sm-2 col-form-label"
+                            for="basic-default-message">Alamat</label>
+                        <div class="col-sm-10">
+                            <textarea name="alamat" class="form-control"
+                                placeholder="Input alamat lengkap"
+                                aria-describedby="basic-icon-default-message2">`+data.alamat+`</textarea>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label class="col-sm-2 col-form-label" for="basic-default-message">Jenis
+                            Kelamin</label>
+                        <div class="col-sm-10">
+                            <select name="jk" id="jkUpdate" class="form-control">
+                                <option selected disabled>Pilih Jenis Kelamin</option>
+                                <option value="Laki-Laki">Laki-Laki</option>
+                                <option value="Perempuan">Perempuan</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label class="col-sm-2 col-form-label" for="basic-default-name">Angkatan</label>
+                        <div class="col-sm-10">
+                            <input type="number" class="form-control" name="angkatan" value="`+data.angkatan+`"
+                                placeholder="Input Tahun Angkatan"> 
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label class="col-sm-2 col-form-label" for="basic-default-name">Kelas</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" name="kelas" value="`+data.kelas+`"
+                                placeholder="Kelas"> 
+                        </div>
+                    </div>
+                    `);
+                    $('#jurusanUpdate').val(data.jurusan);
+                    $('#jkUpdate').val(data.jk);
+                },
+                error: function(result) {
+                    let data = result.responseJSON
+                    Swal.fire({
+                        icon: data.response.icon,
+                        title: data.response.title,
+                        text: data.response.message,
+                    });
+                }
+
             });
         });
+        $('#buttonUpdate').on('click', function() {
+            let dataId = $('#mahasiswaId').val();
+            let url = {!! json_encode(url('/api/mahasiswa')) !!} + "/" + dataId;
+            let data = $('#formUpdate').serialize();
+            $.ajax({
+                url: url,
+                method: "patch",
+                data: data,
+                success: function(result) {
+                    Swal.fire({
+                        title: result.response.title,
+                        text: result.response.message,
+                        icon: result.response.icon,
+                        cancelButtonColor: '#d33',
+                        confirmButtonText: 'Oke'
+                    }).then((result) => {
+                        location.reload();
+                    });
+                },
+                error: function(result) {
+                    let data = result.responseJSON
+                    Swal.fire({
+                        icon: data.response.icon,
+                        title: data.response.title,
+                        text: data.response.message,
+                    });
+                }
+            });
+        });
+    });
 </script>
 @endsection
