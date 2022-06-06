5e355f53cb8a99a1ffec4f03a45e47ae3948b769
IPKIN9 <irwandirini@gmail.com>
2022-06-06 12:38:33 +0800

    Register: New repository interface

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,9 +2,11 @@
 
 namespace App\Providers;
 
+use App\Interfaces\AdminRepositoryInterface;
 use App\Interfaces\DosenRepositoryInterface;
 use App\Interfaces\MahasiswaRepositoryInterface;
 use App\Interfaces\SIRepositoryInterface;
+use App\Repositories\AdminRepository;
 use App\Repositories\DosenRepository;
 use App\Repositories\MahasiswaRepository;
 use App\Repositories\SIRepository;
@@ -22,6 +24,7 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(MahasiswaRepositoryInterface::class, MahasiswaRepository::class);
         $this->app->bind(DosenRepositoryInterface::class, DosenRepository::class);
         $this->app->bind(SIRepositoryInterface::class, SIRepository::class);
+        $this->app->bind(AdminRepositoryInterface::class, AdminRepository::class);
     }
 
     /**
