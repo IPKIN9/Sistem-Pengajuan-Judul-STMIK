c0aa1cfbebd084cee36df57cdeb78f115ca790a6
IPKIN9 <irwandirini@gmail.com>
2022-06-06 08:57:22 +0800

    Add: create Sistem Informasi repository function

--- app/Repositories/SIRepository.php
+++ app/Repositories/SIRepository.php
@@ -18,8 +18,32 @@ class SIRepository implements SIRepositoryInterface
     {
     }
 
-    public function createSI($SI_id, array $newDetails)
+    public function createSI(array $newDetails)
     {
+        try {
+            $dbResult = SisteminformasiModel::create($newDetails);
+            $SI = array(
+                'data' => $dbResult,
+                'response' => array(
+                    'icon' => 'success',
+                    'title' => 'Tersimpan',
+                    'message' => 'Data berhasil disimpan',
+                ),
+                'code' => 201
+            );
+        } catch (\Throwable $th) {
+            $SI = array([
+                'data' => null,
+                'response' => array(
+                    'icon' => 'error',
+                    'title' => 'Gagal',
+                    'message' => $th->getMessage(),
+                ),
+                'code' => 500
+            ]);
+        }
+
+        return $SI;
     }
 
     public function updateSI($SI_id, array $newDetails)
