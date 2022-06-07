2be2d9f671af0efa57eb0b3d82eebdbbcd528d96
IPKIN9 <irwandirini@gmail.com>
2022-06-07 08:58:10 +0800

    Add: Admin create data function

--- app/Repositories/AdminRepository.php
+++ app/Repositories/AdminRepository.php
@@ -24,6 +24,30 @@ class AdminRepository implements AdminRepositoryInterface
 
     public function createAdmin(array $adminDetails)
     {
+        try {
+            $dbResult = AdminModel::create($adminDetails);
+            $admin = array(
+                'data' => $dbResult,
+                'response' => array(
+                    'icon' => 'success',
+                    'title' => 'Tersimpan',
+                    'message' => 'Data berhasil disimpan',
+                ),
+                'code' => 201
+            );
+        } catch (\Throwable $th) {
+            $admin = array(
+                'data' => null,
+                'response' => array(
+                    'icon' => 'error',
+                    'title' => 'Gagal',
+                    'message' => $th->getMessage(),
+                ),
+                'code' => 500
+            );
+        }
+
+        return $admin;
     }
 
     public function updateAdmin($admin_id, array $newDetails)
