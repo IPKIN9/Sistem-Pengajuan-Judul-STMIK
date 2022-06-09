318e1970172a0104fae44c526f2dc5feb34cebba
IPKIN9 <irwandirini@gmail.com>
2022-06-09 10:29:18 +0800

    Add: Judul request validation

--- app/Http/Requests/JudulRequest.php
+++ app/Http/Requests/JudulRequest.php
@@ -2,29 +2,39 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class JudulRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
+
     public function authorize()
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
     public function rules()
     {
         return [
-            //
+            'id_mahasiswa' => 'required|integer',
+            'nama_judul' => 'required|min:2',
+            'descJudul' => 'required|min:2',
         ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'response' => array(
+                'icon' => 'error',
+                'title' => 'Validasi Gagal',
+                'message' => 'Data yang di input tidak tervalidasi',
+            ),
+            'errors' => array(
+                'length' => count($validator->errors()),
+                'data' => $validator->errors()
+            ),
+        ], 422));
     }
 }
