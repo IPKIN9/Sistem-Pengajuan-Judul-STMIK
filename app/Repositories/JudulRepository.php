1fbd975efd9e6bd8c1ccd5f9c46147f85e32a76e
IPKIN9 <irwandirini@gmail.com>
2022-06-09 10:29:39 +0800

    Add: Create judul function

--- app/Repositories/JudulRepository.php
+++ app/Repositories/JudulRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories;
 
 use App\Interfaces\JudulRepositoryInterface;
 use App\Models\JudulModel;
+use App\Models\MahasiswaModel;
 
 class JudulRepository implements JudulRepositoryInterface
 {
@@ -20,6 +21,43 @@ class JudulRepository implements JudulRepositoryInterface
 
     public function createJudul(array $judulDetail)
     {
+        try {
+            $findMahasiswa = MahasiswaModel::whereId($judulDetail['id_mahasiswa'])->first();
+            if ($findMahasiswa) {
+                $dbResult = JudulModel::create($judulDetail);
+                $judul = array(
+                    'data' => $dbResult,
+                    'response' => array(
+                        'icon' => 'success',
+                        'title' => 'Tersimpan',
+                        'message' => 'Data berhasil disimpan',
+                    ),
+                    'code' => 201
+                );
+            } else {
+                $judul = array(
+                    'data' => null,
+                    'response' => array(
+                        'icon' => 'warning',
+                        'title' => 'Not Found',
+                        'message' => 'Data mahasiswa tersedia',
+                    ),
+                    'code' => 404
+                );
+            }
+        } catch (\Throwable $th) {
+            $judul = array(
+                'data' => null,
+                'response' => array(
+                    'icon' => 'error',
+                    'title' => 'Gagal',
+                    'message' => $th->getMessage(),
+                ),
+                'code' => 500
+            );
+        }
+
+        return $judul;
     }
 
     public function updateJudul($judul_id, array $newDetail)
