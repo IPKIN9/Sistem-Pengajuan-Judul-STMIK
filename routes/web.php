950a70acac5274a80935b04d4431aa8478bd4438
husnulmaniah <husnulmaniah86@gmail.com>
2022-06-10 22:23:43 +0800

    modified routes jurnal

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,9 @@ Route::get('/swagger-doc', function () {
     return view('Swagger.index');
 });
 
-Route::get('/mahasiswa', [MahasiswaController::class, 'index'])->name('mahasiswa.index');
-Route::get('/dosen', [DosenController::class, 'index'])->name('dosen.index');
-Route::get('/sistem_informasi', [SIController::class, 'index'])->name('si.index');
-Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
-Route::get('/judul', [JudulController::class, 'index'])->name('judul.index');
-Route::get('/jurnal', [JurnalController::class, 'index'])->name('jurnal.index');
+Route::get('/mahasiswa_page', [MahasiswaController::class, 'index'])->name('mahasiswa.index');
+Route::get('/dosen_page', [DosenController::class, 'index'])->name('dosen.index');
+Route::get('/sistem_informasi_page', [SIController::class, 'index'])->name('si.index');
+Route::get('/admin_page', [AdminController::class, 'index'])->name('admin.index');
+Route::get('/judul_page', [JudulController::class, 'index'])->name('judul.index');
+Route::get('/jurnal_page', [JurnalController::class, 'index'])->name('jurnal.index');
