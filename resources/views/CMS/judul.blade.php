e880899629d713e62e6e371886a747324fe4056f
husnulmaniah <husnulmaniah86@gmail.com>
2022-06-11 14:57:26 +0800

    delete button Edit and delete form create

--- resources/views/CMS/judul.blade.php
+++ resources/views/CMS/judul.blade.php
@@ -3,27 +3,11 @@
 <div class="container-xxl flex-grow-1 container-p-y">
     <div class="row">
         <div class="col-xl-12">
-            <h4 class="text-muted">Data Dosen</h4>
+            <h4 class="text-muted">Data Judul Mahasiswa</h4>
             <div class="nav-align-top mb-4">
-                <ul class="nav nav-pills mb-3 nav-fill" role="tablist">
-                    <li class="nav-item">
-                        <button type="button" class="nav-link active" role="tab" data-bs-toggle="tab"
-                            data-bs-target="#navs-pills-justified-home" aria-controls="navs-pills-justified-home"
-                            aria-selected="true">
-                            <i class="tf-icons bx bx-home"></i> Data Table
-                        </button>
-                    </li>
-                    <li class="nav-item">
-                        <button type="button" class="nav-link" role="tab" data-bs-toggle="tab"
-                            data-bs-target="#navs-pills-justified-profile" aria-controls="navs-pills-justified-profile"
-                            aria-selected="false">
-                            <i class="tf-icons bx bx-user"></i> Input Data
-                        </button>
-                    </li>
-                </ul>
                 <div class="tab-content">
-                    <div class="tab-pane fade show active" id="navs-pills-justified-home" role="tabpanel">
-                        <div class="table-responsive text-nowrap">
+                    <div class="tab-pane fade show active" id="navs-pills-justified-home">
+                        <div class="table-responsive">
                             <table class="table" id="table">
                                 <thead>
                                     <tr class="text-nowrap">
@@ -45,65 +29,18 @@
                                         <td>{{ $d->nama_judul }}</td>
                                         <td>{{ $d->descJudul }}</td>
                                         <td>
-                                            <button type="button" id="editId" data-id="{{ $d->id }}"
+                                            <button type="button" id="infoId" data-id="{{ $d->id }}"
                                                 data-bs-toggle="modal" data-bs-target="#modalUpdate"
-                                                class="btn rounded-pill btn-outline-primary"><i class='bx bx-message-square-edit'></i></button>
+                                                class="btn rounded-pill btn-outline-primary"><i
+                                                    class='bx bxs-low-vision'></i></button>
                                             <button type="button" id="deleteId" data-id="{{ $d->id }}"
-                                                class="btn rounded-pill btn-outline-danger"><i class='bx bx-trash'></i></button>
+                                                class="btn rounded-pill btn-outline-danger"><i
+                                                    class='bx bx-trash'></i></button>
                                         </td>
                                     </tr>
                                     @endforeach
                                 </tbody>
                             </table>
-                        </div>
-                    </div>
-                    <div class="tab-pane fade" id="navs-pills-justified-profile" role="tabpanel">
-                        <div class="col-xxl">
-                            <div class="card-header d-flex align-items-center justify-content-between">
-                                <h5 class="mb-0">Input Data Judul</h5>
-                            </div>
-                            <div class="card-body">
-                                <form id="formSimpan">
-                                    @csrf
-                                    <div class="row mb-3">
-                                        <label class="col-sm-2 col-form-label" for="basic-default-message">Nama
-                                            Mahasiswa</label>
-                                        <div class="col-sm-10">
-                                            <select name="id_mahasiswa" id="id_mahasiswa" class="form-control">
-                                                <option selected disabled>Pilih Nama Mahasiswa</option>
-                                                @foreach ($judul ['mahasiswa'] as $d)
-                                                <option value="{{$d->id}}">{{$d->nama}}</option>
-                                                @endforeach
-                                            </select>
-                                            <p class="text-danger miniAlert text-capitalize" id="alertIdMahasiswa"></p>
-                                        </div>
-                                    </div>
-                                    <div class="row mb-3">
-                                        <label class="col-sm-2 col-form-label" for="basic-default-name">Nama
-                                            Judul</label>
-                                        <div class="col-sm-10">
-                                            <input type="text" class="form-control" name="nama_judul" id="nama_judul"
-                                                placeholder="Input Nama Judul">
-                                            <p class="text-danger miniAlert text-capitalize" id="alertNamaJudul"></p>
-                                        </div>
-                                    </div>
-                                    <div class="row mb-3">
-                                        <label class="col-sm-2 col-form-label" for="basic-default-message">Deskripsi
-                                            Judul</label>
-                                        <div class="col-sm-10">
-                                            <textarea name="descJudul" id="descJudul" class="form-control"
-                                                placeholder="Input Deskripsi judul"
-                                                aria-describedby="basic-icon-default-message2"></textarea>
-                                            <p class="text-danger miniAlert text-capitalize" id="alertDescJudul"></p>
-                                        </div>
-                                    </div>
-                                    <div class="row justify-content-end">
-                                        <div class="col-sm-10">
-                                            <button type="button" class="btn btn-primary" id="saveId">Save</button>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
                         </div>
                     </div>
                 </div>
@@ -122,44 +59,8 @@
             });
 
             $('#table').DataTable();
-            
-            $('#saveId').on('click', function() {
-                let url = `{{ config('app.url') }}` + "/api/judul";
-                let data = $('#formSimpan').serialize();
-                $.ajax({
-                    url: url,
-                    method: "POST",
-                    data: data,
-                    success: function(result) {
-                        Swal.fire({
-                            title: result.response.title,
-                            text: result.response.message,
-                            icon: result.response.icon,
-                            cancelButtonColor: '#d33',
-                            confirmButtonText: 'Oke'
-                        }).then((result) => {
-                            location.reload();
-                        });
-                    },
-                    error: function(result) {
-                        let data = result.responseJSON
-                        let errorRes = data.errors
-                        Swal.fire({
-                            icon: data.response.icon,
-                            title: data.response.title,
-                            text: data.response.message,
-                        });
-                        if (errorRes.length >= 1) {
-                            $('.miniAlert').html('');
-                            $('#alertIdMahasiswa').html(errorRes.data.id_mahasiswa);
-                            $('#alertNamaJudul').html(errorRes.data.nama_judul);
-                            $('#alertDescJudul').html(errorRes.data.descJudul);
-                        }
-                    }
-                });
-            });
-
-            $(document).on('click', '#editId', function() {
+             
+            $(document).on('click', '#infoId', function() {
                 let dataId = $(this).data('id');
                 let url = `{{ config('app.url') }}` + "/api/judul/" + dataId;
                 $.ajax({
@@ -167,8 +68,9 @@
                     type: 'GET',
                     success: function(result) {
                         let data = result.data;
+                        $("#buttonUpdate").remove();
                         $('#modalUpdate').modal('show');
-                        $('.modal-title').html('Perubahan Data');
+                        $('.modal-title').html('Info Data');
                         $('#formUpdate').html('');
                         $('#formUpdate').append(`
                             <div class="row mb-3">
@@ -201,49 +103,7 @@
                             `);
                             $('#id_mahasiswaUpdate').val(data.id_mahasiswa);
                     },
-                    error: function(result) {
-                        let data = result.responseJSON
-                        Swal.fire({
-                            icon: data.response.icon,
-                            title: data.response.title,
-                            text: data.response.message,
-                        });
-                    }
-
-                });
-            });
-            $('#buttonUpdate').on('click', function() {
-                let dataId = $('#judulId').val();
-                let url = `{{ config('app.url') }}` + "/api/judul/" + dataId;
-                let data = $('#formUpdate').serialize();
-                let modalClose = () => {
-                    $('#modalUpdate').modal('hide');
-                }
-                $.ajax({
-                    url: url,
-                    method: "patch",
-                    data: data,
-                    success: function(result) {
-                        modalClose();
-                        Swal.fire({
-                            title: result.response.title,
-                            text: result.response.message,
-                            icon: result.response.icon,
-                            cancelButtonColor: '#d33',
-                            confirmButtonText: 'Oke'
-                        }).then((result) => {
-                            location.reload();
-                        });
-                    },
-                    error: function(result) {
-                        let data = result.responseJSON
-                        modalClose();
-                        Swal.fire({
-                            icon: data.response.icon,
-                            title: data.response.title,
-                            text: data.response.message,
-                        });
-                    }
+                    
                 });
             });
 
