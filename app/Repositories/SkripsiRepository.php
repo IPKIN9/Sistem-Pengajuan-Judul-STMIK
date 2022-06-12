e4462aeebf6cbae498d0680db4e40ad194b75837
IPKIN9 <irwandirini@gmail.com>
2022-06-12 16:03:27 +0800

    Add: Update skripsi function

--- app/Repositories/SkripsiRepository.php
+++ app/Repositories/SkripsiRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories;
 
 use App\Interfaces\SkripsiRepositoryInterface;
 use App\Models\SkripsiModel;
+use Carbon\Carbon;
 
 class SkripsiRepository implements SkripsiRepositoryInterface
 {
@@ -73,6 +74,46 @@ class SkripsiRepository implements SkripsiRepositoryInterface
 
     public function updateSkripsi($skrips_id, array $newDetail)
     {
+        $date = Carbon::now();
+        $newDetail['updated_at'] = $date;
+        try {
+            $dbResult = SkripsiModel::whereId($skrips_id);
+            $findId = $dbResult->first();
+
+            if ($findId) {
+                $skripsi = array(
+                    'data' => $dbResult->update($newDetail),
+                    'response' => array(
+                        'icon' => 'success',
+                        'title' => 'Tersimpan',
+                        'message' => 'Data berhasil disimpan',
+                    ),
+                    'code' => 201
+                );
+            } else {
+                $skripsi = array(
+                    'data' => null,
+                    'response' => array(
+                        'icon' => 'error',
+                        'title' => 'Gagal',
+                        'message' => 'Data tidak ditemukan',
+                    ),
+                    'code' => 404
+                );
+            }
+        } catch (\Throwable $th) {
+            $skripsi = array(
+                'data' => null,
+                'response' => array(
+                    'icon' => 'error',
+                    'title' => 'Gagal',
+                    'message' => $th->getMessage(),
+                ),
+                'code' => 500
+            );
+        }
+
+        return $skripsi;
     }
 
     public function deleteSkripsi($skrips_id)
