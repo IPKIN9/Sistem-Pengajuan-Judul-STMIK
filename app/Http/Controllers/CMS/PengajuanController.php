aaa56e92a6af34ca9954ed6450a5d4a580331824
IPKIN9 <irwandirini@gmail.com>
2022-06-12 08:54:36 +0800

    Create: Pengajuan controller

--- app/Http/Controllers/CMS/PengajuanController.php
+++ app/Http/Controllers/CMS/PengajuanController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\CMS;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\PengajuanRequest;
 use App\Interfaces\PengajuanRepositoryInterface;
 use Illuminate\Http\Request;
 
@@ -16,6 +17,19 @@ class PengajuanController extends Controller
     public function index()
     {
         $pengajuan = $this->pengajuanRepository->getPengajuanAll();
+
+        return response()->json($pengajuan);
+    }
+
+    public function createData(PengajuanRequest $request)
+    {
+        $pengajuanDetail = $request->only(
+            'id_mahasiswa',
+            'id_judul',
+            'status',
+            'detail_tanggal'
+        );
+        $pengajuan = $this->pengajuanRepository->createPengajuan($pengajuanDetail);
 
         return response()->json($pengajuan);
     }
