6143168fe582f0438338c0e262141c46cf7033d0
IPKIN9 <irwandirini@gmail.com>
2022-06-14 18:29:38 +0800

    Config auth

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -1,18 +1,11 @@
 <!DOCTYPE html>
-<html
-  lang="en"
-  class="light-style customizer-hide"
-  dir="ltr"
-  data-theme="theme-default"
-  data-assets-path="../assets/"
-  data-template="vertical-menu-template-free"
->
-  <head>
+<html lang="en" class="light-style customizer-hide" dir="ltr" data-theme="theme-default" data-assets-path="../assets/"
+    data-template="vertical-menu-template-free">
+
+<head>
     <meta charset="utf-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0"
-    />
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
     <title>Login</title>
 
@@ -25,9 +18,8 @@
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link
-      href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
-      rel="stylesheet"
-    />
+        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+        rel="stylesheet" />
 
     <!-- Icons. Uncomment required icon fonts -->
     <link rel="stylesheet" href="../assets/vendor/fonts/boxicons.css" />
@@ -49,55 +41,54 @@
     <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
     <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
     <script src="../assets/js/config.js"></script>
-  </head>
+</head>
 
-  <body>
+<body>
     <!-- Content -->
 
     <div class="container-xxl">
-      <div class="authentication-wrapper authentication-basic container-p-y">
-        <div class="authentication-inner">
-          <!-- Register -->
-          <div class="card">
-            <div class="card-body">
-              <!-- /Logo -->
-              <h4 class="mb-2">Welcome 👋</h4>
-              <p class="mb-4">Please sign-in to your account</p>
-
-              <form id="formAuthentication" class="mb-3" action="index.html" method="POST">
-                <div class="mb-3">
-                  <label for="username" class="form-label">Username</label>
-                  <input
-                    type="text"
-                    class="form-control"
-                    id="email"
-                    name="email-username"
-                    placeholder="Enter your email or username"
-                    autofocus
-                  />
+        <div class="authentication-wrapper authentication-basic container-p-y">
+            <div class="authentication-inner">
+                <!-- Register -->
+                <div class="card">
+                    <div class="card-body">
+                        <!-- /Logo -->
+                        <h4 class="mb-2">Welcome 👋</h4>
+                        <p class="mb-4">Please sign-in to your account</p>
+                        @if (count($errors) > 0)
+                            <div class="alert alert-danger mb-4">
+                                <strong>Whoops!</strong> Ada yang salah dengan inputan anda.
+                                <br>
+                                @foreach ($errors->all() as $error)
+                                    <p style="margin-bottom: -5px">{{ $error }}</p>
+                                @endforeach
+                            </div>
+                        @endif
+                        <form id="formAuthentication" class="mb-3" action="{{ route('login.check') }}"
+                            method="POST">
+                            @csrf
+                            <div class="mb-3">
+                                <label for="username" class="form-label">Username</label>
+                                <input type="text" class="form-control" id="username" name="username"
+                                    placeholder="Enter your username" autocomplete="off" autofocus />
+                            </div>
+                            <div class="mb-3 form-password-toggle">
+                                <div class="d-flex justify-content-between">
+                                    <label class="form-label" for="password">Password</label>
+                                </div>
+                                <div class="input-group input-group-merge">
+                                    <input type="password" id="password" class="form-control" name="password" />
+                                </div>
+                            </div>
+                            <div class="mb-3">
+                                <button class="btn btn-primary d-grid w-100" type="submit">Sign in</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-                <div class="mb-3 form-password-toggle">
-                  <div class="d-flex justify-content-between">
-                    <label class="form-label" for="password">Password</label>
-                  </div>
-                  <div class="input-group input-group-merge">
-                    <input
-                      type="password"
-                      id="password"
-                      class="form-control"
-                      name="password"
-                    />
-                  </div>
-                </div>
-                <div class="mb-3">
-                  <button class="btn btn-primary d-grid w-100" type="submit">Sign in</button>
-                </div>
-              </form>
+                <!-- /Register -->
             </div>
-          </div>
-          <!-- /Register -->
         </div>
-      </div>
     </div>
 
     <!-- Core JS -->
@@ -119,5 +110,6 @@
 
     <!-- Place this tag in your head or just before your close body tag. -->
     <script async defer src="https://buttons.github.io/buttons.js"></script>
-  </body>
+</body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\CMS\AdminController;
 use App\Http\Controllers\CMS\DosenController;
 use App\Http\Controllers\CMS\JudulController;
@@ -10,20 +11,84 @@ use App\Http\Controllers\CMS\SIController;
 use App\Http\Controllers\CMS\SkripsiController;
 use Illuminate\Support\Facades\Route;
 
+Route::prefix('auth')->group(function () {
+    Route::get('/', [AuthController::class, 'index'])->name('login');
+    Route::post('/', [AuthController::class, 'checkCredential'])->name('login.check');
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+});
+Route::middleware('auth')->group(function () {
+    route::get('/', function () {
+        return view('CMS.dashboard');
+    })->name('home');
 
-route::get('/', function () {
-    return view('CMS.dashboard');
+    Route::get('/swagger-doc', function () {
+        return view('Swagger.index');
+    });
+
+    Route::get('/mahasiswa_page', [MahasiswaController::class, 'index'])->name('mahasiswa.index');
+    Route::get('/dosen_page', [DosenController::class, 'index'])->name('dosen.index');
+    Route::get('/sistem_informasi_page', [SIController::class, 'index'])->name('si.index');
+    Route::get('/admin_page', [AdminController::class, 'index'])->name('admin.index');
+    Route::get('/judul_page', [JudulController::class, 'index'])->name('judul.index');
+    Route::get('/jurnal_page', [JurnalController::class, 'index'])->name('jurnal.index');
+    Route::get('/pengajuan_page', [PengajuanController::class, 'index'])->name('pengajuan.index');
+    Route::get('/skripsi_page', [SkripsiController::class, 'index'])->name('skripsi.index');
 });
 
-Route::get('/swagger-doc', function () {
-    return view('Swagger.index');
-});
+Route::prefix('api')->middleware('auth')->group(function () {
+    Route::prefix('mahasiswa')->group(function () {
+        Route::post('/', [MahasiswaController::class, 'createMahasiswa']);
+        Route::get('/{id}', [MahasiswaController::class, 'getById']);
+        Route::patch('/{id}', [MahasiswaController::class, 'updateMahasiswa']);
+        Route::delete('/{id}', [MahasiswaController::class, 'deleteMahasiswa']);
+    });
 
-Route::get('/mahasiswa_page', [MahasiswaController::class, 'index'])->name('mahasiswa.index');
-Route::get('/dosen_page', [DosenController::class, 'index'])->name('dosen.index');
-Route::get('/sistem_informasi_page', [SIController::class, 'index'])->name('si.index');
-Route::get('/admin_page', [AdminController::class, 'index'])->name('admin.index');
-Route::get('/judul_page', [JudulController::class, 'index'])->name('judul.index');
-Route::get('/jurnal_page', [JurnalController::class, 'index'])->name('jurnal.index');
-Route::get('/pengajuan_page', [PengajuanController::class, 'index'])->name('pengajuan.index');
-Route::get('/skripsi_page', [SkripsiController::class, 'index'])->name('skripsi.index');
+    Route::prefix('dosen')->group(function () {
+        Route::post('/', [DosenController::class, 'createInDosen']);
+        Route::get('/{id}', [DosenController::class, 'getById']);
+        Route::patch('/{id}', [DosenController::class, 'updateById']);
+        Route::delete('/{id}', [DosenController::class, 'deleteById']);
+    });
+
+    Route::prefix('sistem_informasi')->group(function () {
+        Route::post('/', [SIController::class, 'createData']);
+        Route::get('/{id}', [SIController::class, 'getById']);
+        Route::patch('/{id}', [SIController::class, 'updateInSI']);
+        Route::delete('/{id}', [SIController::class, 'deleteData']);
+    });
+
+    Route::prefix('admin')->group(function () {
+        Route::post('/', [AdminController::class, 'createData']);
+        Route::get('/{id}', [AdminController::class, 'getById']);
+        Route::patch('/{id}', [AdminController::class, 'updateData']);
+        Route::delete('/{id}', [AdminController::class, 'deleteData']);
+    });
+
+    Route::prefix('judul')->group(function () {
+        Route::post('/', [JudulController::class, 'createData']);
+        Route::get('/{id}', [JudulController::class, 'getById']);
+        Route::patch('/{id}', [JudulController::class, 'updateData']);
+        Route::delete('/{id}', [JudulController::class, 'deleteData']);
+    });
+
+    Route::prefix('jurnal')->group(function () {
+        Route::post('/', [JurnalController::class, 'createData']);
+        Route::get('/{id}', [JurnalController::class, 'getById']);
+        Route::patch('/{id}', [JurnalController::class, 'updateData']);
+        Route::delete('/{id}', [JurnalController::class, 'deleteData']);
+    });
+
+    Route::prefix('pengajuan')->group(function () {
+        Route::post('/', [PengajuanController::class, 'createData']);
+        Route::get('/{id}', [PengajuanController::class, 'getById']);
+        Route::patch('/{id}', [PengajuanController::class, 'updateData']);
+        Route::delete('/{id}', [PengajuanController::class, 'deleteData']);
+    });
+
+    Route::prefix('skripsi')->group(function () {
+        Route::post('/', [SkripsiController::class, 'createData']);
+        Route::get('/{id}', [SkripsiController::class, 'getById']);
+        Route::patch('/{id}', [SkripsiController::class, 'updateData']);
+        Route::delete('/{id}', [SkripsiController::class, 'deleteData']);
+    });
+});
