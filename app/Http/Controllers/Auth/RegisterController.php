9ed3f1694a62841b256fbb5716704caaf24f66ef
husnulmaniah <husnulmaniah86@gmail.com>
2022-06-19 22:45:44 +0800

    Data Akun

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,23 +3,69 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Interfaces\AdminRepositoryInterface;
+use App\Interfaces\DosenRepositoryInterface;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
+    public function __construct(AdminRepositoryInterface $adminRepository, DosenRepositoryInterface $dosenRepository)
+    {
+        $this->adminRepository = $adminRepository;
+        $this->dosenRepository = $dosenRepository;
+    }
+
     public function index()
     {
+        $user = User::all();
+        return view('Auth.regisAkun')->with('user',$user);
     }
 
-    public function createUser(Request $request)
+    public function getByAdmin()
     {
+        $admin = $this->adminRepository->getAllAdmin();
+        return response()->json($admin);
     }
 
-    public function updateUser(Request $request)
+    public function getByDosen()
     {
+        $dosen = $this->dosenRepository->getAllDosen();
+        return response()->json($dosen);
     }
 
-    public function deleteUser($id)
+    public function createAkun(Request $request)
     {
+        // app()[PermissionRegistrar::class]->forgetCachedPermissions();
+        try {
+            $dbResult = User::create([
+                'id_profile' => $request->id_profile,
+                'username' => $request->username,
+                'password' => Hash::make($request->password)
+            ]);
+            $dbResult->assignRole('dosen');
+            $akun = array(
+                'data' => $dbResult,
+                'response' => array(
+                    'icon' => 'success',
+                    'title' => 'Tersimpan',
+                    'message' => 'Data berhasil disimpan',
+                ),
+                'code' => 201
+            );
+        } catch (\Throwable $th) {
+            $akun = array(
+                'data' => null,
+                'response' => array(
+                    'icon' => 'error',
+                    'title' => 'Gagal',
+                    'message' => $th->getMessage(),
+                ),
+                'code' => 500
+            );
+        }
+
+        return response()->json($akun, $akun['code']);
     }
 }
