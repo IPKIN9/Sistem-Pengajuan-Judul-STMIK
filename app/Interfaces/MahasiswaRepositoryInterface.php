d3370081477efea82b2e620c935d028f7dd68529
IPKIN9 <irwandirini@gmail.com>
2022-07-13 12:24:36 +0800

    Bug done

--- app/Interfaces/MahasiswaRepositoryInterface.php
+++ app/Interfaces/MahasiswaRepositoryInterface.php
@@ -6,7 +6,7 @@ interface MahasiswaRepositoryInterface
 {
     public function getAllMahasiswa();
     public function getMahasiswaById($mahasiswaId);
-    public function getMahasiswaByNim($nim);
+    public function getMahasiswaByNim($nim, $idtanggal);
     public function createMahasiswa(array $mahasiswaDetails);
     public function updateMahasiswaById($mahasiswa_id, array $newDetails);
     public function deleteMahasiswa($mahasiswaId);
